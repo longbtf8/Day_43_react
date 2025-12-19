8440657a6a4afc0e9966eaa896de9d27413d31eb
longbtf8 <longbtf8@fullstack.edu.vn>
2025-12-20 03:37:24 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
 import AuthLayout from "./components/AuthLayout";
-import DefaultLayout from "./components/DefaultLayout copy";
+import DefaultLayout from "./components/DefaultLayout";
 import Home from "./pages/Home";
 import HOCDemo from "./pages/src/pages/HOCDemo";
 import RenderPropsDemo from "./pages/RenderPropsDemo";
